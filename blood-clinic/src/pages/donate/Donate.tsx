a9e064ecb15914e3ed3a4d2c35d6a7fe6d130e92
Madalin Marian Porojan <madalin.porojan2000@gmail.com>
2023-05-21 12:04:01 +0300

    styling

--- blood-clinic/src/pages/donate/Donate.tsx
+++ blood-clinic/src/pages/donate/Donate.tsx
@@ -18,8 +18,11 @@ function Donate() {
             <div className="mx-auto max-w-screen-md px-4 pt-8 lg:py-16">
                 <h2 className="text-center text-4xl text-gray-900 dark:text-white">
                     <span className="bg-gradient-to-r from-red-600 to-blue-800 bg-clip-text font-medium tracking-tighter text-transparent"> 
-                        <span className="font-bold"> 
-                        Schedule Your Appointment
+                        <span className="font-medium"> 
+                        Schedule Your&nbsp;
+                            <span className="font-extrabold"> 
+                             Appointment
+                            </span>
                         </span>
                     </span>
                 </h2>
@@ -74,11 +77,11 @@ function Donate() {
                     <div className="grid md:grid-cols-2 md:gap-6">
                         <div className="group relative z-0 mb-6 w-full">
                             <input type="text" name="floating_contact_full_name" id="floating_contact_full_name" className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500" placeholder=" " required />
-                            <label htmlFor="floating_contact_full_name" className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">Contact Person Full Name</label>
+                            <label htmlFor="floating_contact_full_name" className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">Emergency Contact Full Name</label>
                         </div>
                         <div className="group relative z-0 mb-6 w-full">
                             <input type="tel" pattern="[0-9]{10}" name="floating_contact_phone" id="floating_contact_phone" className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500" placeholder=" " required />
-                            <label htmlFor="floating_contact_phone" className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">Contact Person Phone number</label>
+                            <label htmlFor="floating_contact_phone" className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">Emergency Contact Phone number</label>
                         </div>
                     </div>
 
@@ -90,7 +93,7 @@ function Donate() {
                             <TimePicker></TimePicker>
                         </div>
                     </div>
-                    <button type="submit" className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">Submit</button>
+                    <button type="submit" className="mt-5 w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">Schedule</button>
 
 
                 </form>
